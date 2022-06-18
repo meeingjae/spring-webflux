719ef49e00c2831d7b62ff5adedace7481606a1a
Minjae Kim <minjae.kim@mytrum.com>
2022-06-18 12:25:37 +0900

    feat: Car 단건 조회 구현
    * Car - getter, setter 추가
    * CarRepository Bean Name 수정

--- src/main/java/com/springwebflux/car/repository/CarRepository.java
+++ src/main/java/com/springwebflux/car/repository/CarRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import com.springwebflux.car.vo.Car;
 
+import reactor.core.publisher.Mono;
+
 @Repository
 public class CarRepository {
     private static final Map<Long, Car> carMap;
@@ -17,5 +19,9 @@ public class CarRepository {
         carMap.put(2L, new Car(2L, "KIA", 25000000));
         carMap.put(3L, new Car(3L, "AUDI", 40000000));
         carMap.put(4L, new Car(4L, "PORSCHE", 90000000));
+    }
+
+    public Mono<Car> getById(long id) {
+        return Mono.just(carMap.get(id) != null ? carMap.get(id) : new Car());
     }
 }
